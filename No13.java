8d1ea4b670a57c9cddc6cca9270adda67e7d490c
jessica-sie <jessica.sie@binus.ac.id>
2022-03-07 10:22:39 +0700

    commit

--- No13.java
+++ No13.java
@@ -10,10 +10,22 @@ public class No13 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("enter your polinomial equation");
-        String poly = sc.nextLine();
+        System.out.println("your polinomial equation in the form of ax^2 + bx + c");
+        System.out.println("a: ");
+        float a = sc.nextFloat();
 
-        // STRING AUGMENTATION
+        System.out.println("b: ");
+        float b = sc.nextFloat();
+
+        System.out.println("c: ");
+        float c = sc.nextFloat();
+
+        // SOLVING FOR X USING [-b +/- sqrt(b^2 - 4ac)]/2a
+        double x1 = (-b + Math.sqrt(b*b - 4*a*c))/2*a;
+        double x2 = (-b - Math.sqrt(b*b - 4*a*c))/2*a;
+
+        System.out.println("solution for the quadratic equation is : " + x1 + " and " + x2);
+
         
 
 
